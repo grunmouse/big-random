333b03b3e43f8cf489af50688afcedef2cfbc64c
Dmitry Rakov <rakov@mirapolis.ru>
2022-09-18 21:05:26 +0300

    Переписал RC4 под себя.

--- test/rc4-test.js
+++ test/rc4-test.js
@@ -25,121 +25,41 @@ var chi6p99 = 16.812;
 describe("constructor", function () {
   it("autoseeds if no parameter given", function () {
     var rc4 = new RC4();
-    var r = rc4.randomFloat();
+    var r = rc4.randomByte();
 
-    assert(0 <= r && r < 1);
+    assert(0 <= r && r < 256);
   });
 
-  it("takes string as seed", function () {
-    var rc4 = new RC4("lol");
-
-    assert.equal(rc4.randomByte(), 65);
-  });
-
-  it("takes array of integers as seed", function () {
-    var rc4 = new RC4([1, 2, 3]);
-
-    assert.equal(rc4.randomByte(), 151);
-  });
-
-  it("throws if given array of non integers", function () {
-    assert.throws(function () {
-      var rc4 = new RC4([Infinity]);
-      rc4.randomByte();
-    });
-
-    assert.throws(function () {
-      var rc4 = new RC4([NaN]);
-      rc4.randomByte();
-    });
-
-    assert.throws(function () {
-      var rc4 = new RC4(["foo"]);
-      rc4.randomByte();
-    });
-  });
-
-  it("throws if given something else", function () {
-    assert.throws(function () {
-      var rc4 = new RC4(123);
-      rc4.randomByte();
-    });
-  });
-});
-
-describe("randomFloat", function () {
-  it("returns values ∈ [0, 1)", function () {
-    var rc4 = new RC4("deadbeef");
-    var N = 1000;
-    var smaller = 0;
-    for (var i = 0; i < N; i++) {
-      var r = rc4.randomFloat();
-      assert(0 <= r && r < 1, "float should be in range of [0, 1)");
-
-      if (r < 0.5) {
-        smaller += 1;
-      }
-    }
-
-    var chi = (sq(smaller - N/2) + sq((N - smaller) - N/2)) / (N/2);
-    assert(chi1p1 < chi && chi < chi1p99);
-  });
 });
 
 describe("currentState + setState", function () {
   it("can be used to reproduce values", function () {
-    var rc4 = new RC4("deadbeef");
+    var rc4 = new RC4();
     var state = rc4.currentState();
     var N = 100;
 
     function generate() {
       var result = new Array(N);
       for (var i =0; i < N; i++) {
-        result[i] = rc4.randomFloat();
+        result[i] = rc4.randomByte();
       }
       return result;
     }
 
     var a = generate();
+	
     rc4.setState(state);
+	
+	assert.deepEqual(rc4.currentState(), state, 'state set success');
     var b = generate();
 
     assert.deepEqual(a, b, "generated sequences should be equal");
   });
 
-  it("throws an error if state is invalid", function () {
-    assert.throws(function () {
-      var rc4 = new RC4("deadbeef");
-      var state = rc4.currentState();
-      state.i = "foo";
-      rc4.setState(state);
-    });
-
-    assert.throws(function () {
-      var rc4 = new RC4("deadbeef");
-      var state = rc4.currentState();
-      state.j = "foo";
-      rc4.setState(state);
-    });
-
-    assert.throws(function () {
-      var rc4 = new RC4("deadbeef");
-      var state = rc4.currentState();
-      state.s.pop();
-      rc4.setState(state);
-    });
-
-    assert.throws(function () {
-      var rc4 = new RC4("deadbeef");
-      var state = rc4.currentState();
-      state.s[0] = state.s[1];
-      rc4.setState(state);
-    });
-  });
 });
 
 describe("χ² tests", function () {
-  var rc4 = new RC4("deadbeef");
+  var rc4 = new RC4();
 
   function run(N) {
       var i;
@@ -161,8 +81,8 @@ describe("χ² tests", function () {
       var chih = 0;
       var chil = 0;
 
+      var nps = N/16;
       for (i = 0; i < 16; i++){
-        var nps = N/16;
         chih += sq(high[i] - nps)/nps;
         chil += sq(low[i] - nps)/nps;
       }
@@ -194,76 +114,10 @@ describe("χ² tests", function () {
   });
 });
 
-describe("random", function () {
-  var rc4 = new RC4("deadbeef");
-
-  function run(N) {
-      var i;
-      var values = new Array(7);
-
-      for (i = 0; i < 7; i++) {
-        values[i] = 0;
-      }
-
-      for (i = 0; i < N; i++) {
-        var b = rc4.random(6); // inclusive, 0..6 = 7 values
-        values[b] += 1;
-      }
-
-      var chi = 0;
-
-      for (i = 0; i < 7; i++){
-        var nps = N/7;
-        chi += sq(values[i] - nps)/nps;
-      }
-
-      return (chi6p1 < chi && chi < chi6p99);
-  }
-
-  function chisqurareit(N, n) {
-    it("N = " + N + " - " + n + ". run", function () {
-      var a = run(N);
-      var b = run(N);
-      var c = run(N);
-
-      // only one may fail
-      assert(a || b);
-      assert(b || c);
-    });
-  }
-
-  [1000, 10000, 100000].forEach(function (N) {
-    [1, 2, 3].forEach(function (n) {
-      chisqurareit(N, n);
-    });
-  });
-
-  it("requires one or two integer arguments", function () {
-    assert.throws(function () {
-      rc4.random();
-    });
-
-    assert.throws(function () {
-      rc4.random("a");
-    });
-
-    assert.throws(function () {
-      rc4.random(0, "a");
-    });
-
-    assert.throws(function () {
-      rc4.random(0, 1, 3);
-    });
-
-    assert.throws(function () {
-      rc4.random(0, 0.5);
-    });
-  });
-});
 
 describe("RC4small", function () {
   it("randomNative returns integers in [0, 15]", function () {
-    var rc4 = new RC4.RC4small("deadbeef");
+    var rc4 = new RC4.RC4small();
     var N = 1000;
 
     for (var i = 0; i < N; i++) {
@@ -273,7 +127,7 @@ describe("RC4small", function () {
   });
 
   it("randomByte returns integers in [0, 255]", function () {
-    var rc4 = new RC4.RC4small("deadbeef");
+    var rc4 = new RC4.RC4small();
     var N = 1000;
 
     for (var i = 0; i < N; i++) {
@@ -284,14 +138,14 @@ describe("RC4small", function () {
 
   describe("currentStateString + setStateString", function () {
     it("can be used to reproduce values", function () {
-      var rc4 = new RC4.RC4small("deadbeef");
+      var rc4 = new RC4.RC4small();
       var state = rc4.currentStateString();
       var N = 100;
 
       function generate() {
         var result = new Array(N);
         for (var i =0; i < N; i++) {
-          result[i] = rc4.randomFloat();
+          result[i] = rc4.randomNative();
         }
         return result;
       }
@@ -305,34 +159,17 @@ describe("RC4small", function () {
 
     it("throws an error if stateString is invalid", function () {
       assert.throws(function () {
-        var rc4 = new RC4.RC4small("deadbeef");
+        var rc4 = new RC4.RC4small();
         rc4.setStateString("abc");
       });
     });
   });
 
-  describe("randomFloat", function () {
-    it("returns values ∈ [0, 1)", function () {
-      var rc4 = new RC4.RC4small("something other");
-      var N = 1000;
-      var smaller = 0;
-      for (var i = 0; i < N; i++) {
-        var r = rc4.randomFloat();
-        assert(0 <= r && r < 1, "float should be in range of [0, 1)");
 
-        if (r < 0.5) {
-          smaller += 1;
-        }
-      }
-
-      var chi = (sq(smaller - N/2) + sq((N - smaller) - N/2)) / (N/2);
-      assert(chi1p1 < chi && chi < chi1p99);
-    });
-  });
 });
 
 describe("χ² tests, RC4small", function () {
-  var rc4 = new RC4.RC4small("deadbeef");
+  var rc4 = new RC4.RC4small();
 
   function run(N) {
       var i;
@@ -349,8 +186,8 @@ describe("χ² tests, RC4small", function () {
 
       var chi = 0;
 
+      var nps = N/16;
       for (i = 0; i < 16; i++){
-        var nps = N/16;
         chi += sq(values[i] - nps)/nps;
       }
 
